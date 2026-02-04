9762748979d9b04b38774d8ebc7bd6c76ad0d616
Weston Lane <weston.r.lane@gmail.com>
2026-02-04 00:10:46 -0500

    test

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -5,7 +5,7 @@ public class DummyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("hello world");
+        Debug.Log("goodbye world");
     }
         
     
