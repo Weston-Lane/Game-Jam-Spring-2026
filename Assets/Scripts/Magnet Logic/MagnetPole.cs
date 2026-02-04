5599100c57983c3d1f9d9c8d6e1df0c966e511ee
Weston Lane <weston.r.lane@gmail.com>
2026-02-04 14:52:26 -0500

    Revert "Merge branch 'Weston'"
    This reverts commit 91d364ea37ca220808640aa45ec8ed88224b4a1b, reversing
    changes made to edd440508655b58e31d564010e35e2db009adf60.

--- Assets/Scripts/Magnet Logic/MagnetPole.cs
+++ Assets/Scripts/Magnet Logic/MagnetPole.cs
@@ -1,66 +1,47 @@
 using System.Data.Common;
-using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class MagnetPole : MonoBehaviour
 {
     #region Inspector Objects
     [Header("Object References")]
-    [SerializeField] Rigidbody PoleRb;
-    [SerializeField] Transform[] neighborPoles;
+    [SerializeField] Rigidbody rb;
 
     [Header("Configuration Variables")]
     [SerializeField] bool isNorth;
-    [SerializeField] float power = 2f;
-    [SerializeField] float fieldRadius = 0.5f;
+    [SerializeField] float power;
 
     //[Header("State")]
     #endregion
 
-    SphereCollider sc;
 
-    public void SetFieldRadius(float radius) => GetComponent<SphereCollider>().radius = radius;
-    public void SetPower(float power) => this.power = power;
-    void Start()
-    {
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody>();    
+    // }
 
-        sc = GetComponent<SphereCollider>();
-        
-    }
-    private void OnValidate()
-    {
-        sc = GetComponent<SphereCollider>();
-        sc.radius = fieldRadius;
-    }
     private void OnTriggerStay(Collider collision)
     {
 
         MagnetPole mp;
         if(collision.transform.TryGetComponent<MagnetPole>(out mp))
         {
-            foreach(var neighbor in neighborPoles)
-                //check if pole is a neighbor
-            {
-                if(collision.transform == neighbor)
-                    { return; }
-            }
-
             Vector3 dirVector = transform.position - collision.transform.position;
             dirVector = dirVector.normalized;
             float dist = Vector3.Distance(transform.position, collision.transform.position);
             Vector3 forceVector =
-                (dirVector / (dist * dist)) * power * mp.power;
+                dirVector / (dist * dist) * power * mp.power;
 
             if(mp.isNorth && isNorth ||
                 !mp.isNorth && !isNorth)
                 //if same pole repel
             {
-                PoleRb.AddForce(forceVector);
+                rb.AddForce(forceVector);
             }
             else
             //else attract
             {
-                PoleRb.AddForce(-forceVector);
+                rb.AddForce(-forceVector);
             }
         }
         else
@@ -68,7 +49,4 @@ public class MagnetPole : MonoBehaviour
             return;
         }
     }
-
-
-
 }
